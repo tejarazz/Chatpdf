241603c7a91d034c6e27e0deff810620f2398b7c
krishna teja <pb.saikrishnateja.com>
2024-01-23 19:18:57 +0530

    changing some colors in ui

--- src/frontend/DB_SETUP/db_setup.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (id, username, email, password_hash)
-VALUES
-    (1, 'krishna teja', 'pb.saikrishnateja@gmail.com', '12345678'),
-    (2, 'teja', 'tejarazz123@gmail.com', '87654321');
-
-select * from users
