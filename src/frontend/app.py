9af6a08fd40eaa46f99dfa4b93c19f686c6223d2
krishna teja <pb.saikrishnateja.com>
2024-02-01 23:55:40 +0530

    frontend dynamic paths

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
+backend_base_path = os.getenv('BACKEND_BASE_PATH', 'http://127.0.0.1:5000/')
 
 
 @app.route('/')
 def openaiTest():
-    return render_template('openai_test.html', params={'username': 'Teja'})
+    return render_template('openai_test.html', params={'username': 'Teja', 'backend_base_path': backend_base_path})
 
 
 if __name__ == '__main__':
